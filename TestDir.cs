3d7025da48ecbc5b843a3b3041ffa97c64820045
TOFGAMES <tofgames555@gmail.com>
2019-05-30 23:14:14 +0900

    testdir

---
+++ TestDir.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class TestDir : MonoBehaviour {
+    private void Update () {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            DirectoryInfo info = new DirectoryInfo(Application.dataPath + @"/" + "test");
+            info.Create();
+        }
+
+        if(Input.GetKeyDown(KeyCode.B)) {
+            DirectoryInfo info = new DirectoryInfo(Application.dataPath + @"/" + "test");
+            info.Create();
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
